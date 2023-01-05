1c4fd42c5b38ab69e7ac27aae80a8fc45a78d171
Aviv Eldad <79564995+AvivEldad@users.noreply.github.com>
2023-01-05 14:47:13 +0200

    Add files via upload

---
+++ general.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from os import path
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Data.CodonTable import TranslationError
+
+DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Data')
+
+DNA = ['A', 'C', 'T', 'G']
+
+TRANS_TABLE_11 = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W'
+}
+
+
+def parse_genbank(gb_path):
+    assert (path.exists(gb_path))
+    with open(gb_path, 'r') as input_handle:
+        gen = SeqIO.parse(input_handle, 'genbank')
+        record_gb = next(gen)
+
+    return record_gb
+
+
+def create_dataframe_from_gb(gb_path, id_header='locus_tag'):
+    record_gb = parse_genbank(gb_path)
+    genes_id, start, end, feat_types, strand, cell_wall = [], [], [], [], [], []
+    tables, translations, codon_starts = [], [], []  # only for cds
+    gene_names = []
+    features = record_gb.features[1:]
+
+    for feature in features:
+        cw = 'No'
+        if id_header in feature.qualifiers.keys():
+            g_name = feature.qualifiers[id_header][0]
+        else:
+            g_name = None
+
+        if feature.type == 'gene':
+            gene_names.append(g_name)
+            continue
+
+        if feature.type == 'CDS':
+            translation = feature.qualifiers['translation'][0]
+            codon_start = feature.qualifiers['codon_start'][0]
+
+            if 'transl_table' in feature.qualifiers.keys():
+                table = feature.qualifiers['transl_table'][0]
+            else:
+                table = 1
+
+            if 'product' in feature.qualifiers.keys():
+                if 'cell wall' in feature.qualifiers['product'][0]:
+                    cw = 'Yes'
+
+        else:
+            table = None
+            translation = None
+            codon_start = None
+
+        genes_id.append(g_name)
+        feat_types.append(feature.type)
+        start.append(feature.location.start)
+        end.append(feature.location.end)
+        strand.append(feature.location.strand)
+        cell_wall.append(cw)
+        tables.append(table)
+        translations.append(translation)
+        codon_starts.append(codon_start)
+
+    df = pd.DataFrame(zip(genes_id, start, end, strand, feat_types, tables, translations, codon_starts, cell_wall),
+                      columns=['id', 'start', 'end', 'strand', 'type', 'table', 'translation', 'codon_start',
+                               'cell wall'])
+
+    sequence = record_gb.seq.upper()  # full genome
+
+    return df, sequence, gene_names
+
+
+def cds_other(df):
+    cds = df[df['type'] == 'CDS']
+    other = df[df['type'] != 'CDS']
+    return cds, other
+
+
+def show_stat(arr, title):
+    if type(arr) != np.ndarray:
+        arr = np.asarray(arr)
+    minimum = np.min(arr)
+    maximum = np.max(arr)
+    avg = np.average(arr)
+    std = np.std(arr)
+
+    print('\033[93m' + f"{title}:\n" + '\033[0m'
+                                       f"Average: {avg:.2f}\n"
+                                       f"Minimum: {minimum:.2f}\n"
+                                       f"Maximum: {maximum:.2f}\n"
+                                       f"Standard deviation: {std:.2f}\n")
+
+
+def plot_hist(title, arr, x_label, y_label, x_max, y_max):
+    plt.title(title)
+    plt.hist(arr, color='purple')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xlim([0, x_max])
+    plt.ylim([0, y_max])
+
+
+def calc_percentage_in_genes(seq, letters):
+    counts = {}
+    for letter in letters:
+        count = seq.count(letter)
+        counts[letter] = count
+
+    return (sum(counts.values()) / len(seq)) * 100
+
+
+def check_translation(gene_type, org_trans, seq, trans_table, strand, codon_start):
+    if gene_type != "CDS":
+        return None
+
+    codon_start = int(codon_start)
+    org_seq = Seq(seq[(codon_start - 1):])
+    if strand != 1:
+        org_seq = org_seq.reverse_complement()
+
+    try:
+        trans = str(org_seq.translate(table=trans_table, cds=True))
+        check = (trans == org_trans)
+        if check:
+            return "OK"
+
+    except TranslationError as err:
+        return str(err)
---
+++ part_b.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from Bio import SeqIO
+from os import path
+import numpy as np
+import matplotlib.pyplot as plt
+import general as ge
+
+
+
+def prepre_data(names):
+    prepred = []
+    nan_count = 0
+    for name in names:
+        if(str(name) == 'nan'):
+            nan_count += 1
+        if(str(name) != 'nan'):
+            name = str(name).replace('_','').replace(';',' ').replace('/', ' ')
+            multi = str(name).split()
+            for i in multi:
+                prepred.append(i)
+    return prepred, nan_count
+
+
+def plot_pie_charts(total, nan_values, uniqe_genes, name):
+    y = np.array([total, nan_values, uniqe_genes])
+    mylabels = ["total", "non-values", "not in gb"]
+
+    plt.pie(y, labels=mylabels, explode=[0.05, 0.05, 0.2], shadow=True, autopct='%1.3f%%')
+    plt.title("{} genes".format(name))
+    plt.show()
+
+
+def Q1():
+    uniprot_names = np.asarray(pd.read_csv('uniprot_BS168.csv')['Gene Names (ordered locus)'])
+    df = pd.read_csv('part_a.csv')
+    gb = df[df['type'] == 'CDS']
+    gb_names = np.asarray(gb['id'])
+
+    # prepre the data
+    un_to_compre, uni_nan = prepre_data(uniprot_names)
+    gb_to_compre, gb_nan = prepre_data(gb_names)
+
+    in_uni = list(set(un_to_compre).difference(gb_to_compre))
+    in_gb = list(set(gb_to_compre).difference(un_to_compre))
+
+    print("uniprot and not in gb:\n{}".format(in_uni))
+    print("\nmissing: {}, nan values: {}, total: {}".format(len(in_uni), uni_nan, len(uniprot_names)))
+    print("gb and not in uniprot:\n{}".format(in_gb))
+    print("\nmissing: {}, nan values: {}, total: {}".format(len(in_gb), gb_nan, len(gb_names)))
+
+    plot_pie_charts(len(uniprot_names), uni_nan, len(in_uni), "Uniprot")
+    plot_pie_charts(len(gb_names), gb_nan, len(in_gb), "GeneBank")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    #Q1
+    # Q1()
+
+    #Q2a
+
+    non_empty_df = pd.read_csv('uniprot_BS168.csv').dropna(subset=['Transmembrane'])
+    start_list, end_list, len_list, gene_list, seq_list = [], [], [], [], []
+    for index, row in non_empty_df.iterrows():
+        name = row['Gene Names (ordered locus)']
+        seq = row['Sequence']
+        trans = row['Transmembrane'].replace('TRANSMEM ', '').replace(' ', '').split(';')
+        trans = [t for t in trans if '\"' not in t]
+        for i in trans:
+            splitter = i.split('..')
+            start, end = int(splitter[0]), int(splitter[1])
+            start_list.append(start)
+            end_list.append(end)
+            len_list.append(np.abs(end - start))
+            gene_list.append(name)
+            seq_list.append(seq[start:end])
+
+    final_df = pd.DataFrame(zip(gene_list, start_list, end_list, len_list, seq_list), columns=['Id', 'Start', 'End', 'Length', 'Sequence'])
+    total_len = np.asarray(final_df['Length'])
+    ge.show_stat(total_len, 'Transmembrane lengths stats')
+    ge.plot_hist("Transmembrane lengths", total_len, 'length', 'count', np.max(total_len) + 5, 8000)
+    plt.show()
+
+    # Q2b
+
+    HYDROPHOBIC_AMINO = ['A', 'F', 'L', 'I', 'V', 'M', 'P', 'W']
+    sequences = final_df['Sequence']
+    seq_prectange = []
+    for seq in sequences:
+        hydro = sum([1 for c in seq if c in HYDROPHOBIC_AMINO])
+        hydro_percent = (hydro / len(seq)) * 100
+        seq_prectange.append(hydro_percent)
+
+    ge.show_stat( seq_prectange, "Hydrophobic Amino Percent in Transmembrane sequences")
+
+
+    
