653e7ca45fd06007a94d96e2ef5c6b1529fa3a64
AvivEldad <avivelded@gmail.com>
2023-01-12 19:12:56 +0200

    part b updates

--- part_B.py
+++ part_B.py
@@ -106,14 +106,14 @@ class UniProt:
         plt.figure(figsize=(15, 6))
 
         plt.subplot(1, 4, 1)
-        ge.plot_hist("GB percent", at_percent_gb, x_title, y_title, x_max, y_max)
+        ge.plot_hist("GenBank (A)", at_percent_gb, x_title, y_title, x_max, y_max)
 
         plt.subplot(1, 4, 2)
-        ge.plot_hist("GB with Trans",
+        ge.plot_hist("GenBank with Trans (B)",
                      intersection_seq_percent, x_title, y_title, x_max, y_max)
 
         plt.subplot(1, 4, 3)
-        ge.plot_hist("GB without Trans",
+        ge.plot_hist("GB without Trans (A/B)",
                      complement_seq_percent, x_title, y_title, x_max, y_max)
 
         plt.subplot(1, 4, 4)
